ff204790e0d9c25aecd7b9c69c7ba9a3653a4852
Daniel Alexandro <danx_97@hotmail.com>
2022-03-31 04:06:41 +0200

    Parallelization

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <queue>
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -14,8 +15,16 @@ int a;
 std::vector<std::vector<int>> G;
 // Minimum edges cut weight sum
 int minEdgeCut = INT32_MAX;
+int bfsMaxLevel = 1;
 // N recursion calls
 int recursionCalls = 0;
+
+struct Node
+{
+    int id;
+    std::vector<bool> visitedNodes;
+};
+std::vector<Node> nodes;
 
 void ParseGraphFile(std::string fileName)
 {
@@ -85,7 +94,7 @@ void DFS(int vertexId, int cut, std::vector<bool> &visitedVertices, std::vector<
     {
         int cutWeight = GetCutTotalWeights(visitedVertices);
 
-        #pragma omp critical 
+        #pragma omp critical
         {
             // Override current minCutWeight sum if better
             if (cutWeight < minEdgeCut)
@@ -112,8 +121,8 @@ void DFS(int vertexId, int cut, std::vector<bool> &visitedVertices, std::vector<
             if ((!visitedVertices[i]) && G[vertexId][i] != 0)
             {
                 int parallelCuts = cut == 1;
-                
-                #pragma omp task if(parallelCuts)
+
+                #pragma omp task if (parallelCuts)
                 {
                     DFS(i, cut + 1, visitedVertices, edgeCuts);
                 }
@@ -123,6 +132,101 @@ void DFS(int vertexId, int cut, std::vector<bool> &visitedVertices, std::vector<
             }
         }
     }
+}
+
+void BFS(int vertexId, int depth, std::vector<bool> &visitedVertices, std::vector<bool> &edgeCuts)
+{
+    std::queue<int> q;
+    q.push(vertexId);
+    visitedVertices[vertexId] = true;
+
+    Node currentNode;
+    for (int i = 0; i < n; i++)
+    {
+        currentNode.visitedNodes.push_back(false);
+    }
+    currentNode.id = vertexId;
+    currentNode.visitedNodes[vertexId] = true;
+    nodes.push_back(currentNode);
+
+    std::vector<int> maxDepthNodes;
+
+    while (depth < bfsMaxLevel)
+    {
+        int nElements = q.size();
+        while (!q.empty() && nElements > 0)
+        {
+            int vertexId = q.front();
+            q.pop();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!visitedVertices[i] && G[vertexId][i] != 0)
+                {
+                    q.push(i);
+                    visitedVertices[i] = true;
+                    maxDepthNodes.push_back(i);
+
+                    // Create a copy of the current visited node
+                    Node visitedNode;
+                    visitedNode.id = i;
+                    for (int j = 0; j < n; j++)
+                    {
+                        visitedNode.visitedNodes.push_back(false);
+                    }
+                    visitedNode.visitedNodes[vertexId] = true;
+                    visitedNode.visitedNodes[i] = true;
+
+                    nodes.push_back(visitedNode);
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (currentNode.visitedNodes[j])
+                        {
+                            visitedNode.visitedNodes[j] = true;
+                        }
+                    }
+
+                    if (depth + 1 < bfsMaxLevel)
+                    {
+                        maxDepthNodes.clear();
+                    }
+                }
+                
+            }
+            nElements--;
+            if (nElements == 0)
+            {
+                depth++;
+            }  
+        }
+    }
+    // DFS
+    int tempMinCut = 0;
+    #pragma omp parallel
+    {
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            for (int j = 0; j < maxDepthNodes.size(); j++)
+            {
+                if (nodes[i].id == maxDepthNodes[j])
+                {
+                    std::vector<bool> tempVisitedNodes;
+                    for (int k = 0; k < n; k++)
+                    {
+                        tempVisitedNodes.push_back(nodes[i].visitedNodes[k]);
+                    }
+                    DFS(nodes[i].id, depth, tempVisitedNodes, edgeCuts);
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
 }
 
 void PrintEdgeCutVertices(std::vector<bool> &edgeCuts)
@@ -181,23 +285,41 @@ int main(int argc, char const *argv[])
     // Begin Timer
     std::chrono::high_resolution_clock::time_point timerStart = std::chrono::high_resolution_clock::now();
 
-    #pragma omp parallel
+    if (a < bfsMaxLevel)
     {
-        #pragma omp single
-        DFS(0, 0, isVertexVisited, isEdgeCut);
+        #pragma omp parallel
+        {
+            #pragma omp single
+            DFS(0, 0, isVertexVisited, isEdgeCut);
+        }
     }
-
+    else
+    {
+        std::queue<int> bfsNodes;
+        bfsNodes.push(0);
+        bfsNodes.push(-1);
+        BFS(0, 0, isVertexVisited, isEdgeCut);
+    }
 
     // End Timer
     std::chrono::high_resolution_clock::time_point timerEnd = std::chrono::high_resolution_clock::now();
 
     std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(timerEnd - timerStart);
 
-    
+    // std::cout<<"AAAAAAAAAA"<< std::endl;
+    // std::cout << "{ ";
+    // for (int i = 0; i < n; i++)
+    //     {
+
+    //         std::cout << isVertexVisited[i] << " ";
+
+    //     }
+    //     std::cout << "}" << std::endl;
+
     // PrintEdgeCutVertices(isEdgeCut);
 
     std::cout << "MinEdgeCut = " << minEdgeCut << std::endl;
-    std::cout << "Time = " << duration.count() << " ms"<< std::endl;
+    std::cout << "Time = " << duration.count() << " ms" << std::endl;
 
     // std::cout << "Recursion calls = " << recursionCalls << std::endl;
 
